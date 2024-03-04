8fec2a7c3d616852bebf73a9eb94c82417e73480
Alisher <aisroilov1029@gmail.com>
2024-03-04 16:09:53 +0500

    tab nav added

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/meal.dart';
 import 'package:meals/screens/meal_details.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, required this.title, required this.meals});
+  const MealsScreen({super.key,   this.title, required this.meals});
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
   void selectMeal(BuildContext context, Meal meal) {
     Navigator.of(context).push(
@@ -55,9 +54,13 @@ class MealsScreen extends StatelessWidget {
         ),
       );
     }
+    if(title == null){
+      return content;
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );
